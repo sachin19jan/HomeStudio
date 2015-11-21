873da2116a807d8551c4ac10dc746c59c53b47e8
Sachin Bhardwaj <sachin007@Sachins-MacBook-Pro.local>
2015-11-21 20:49:17 +0530

    App delegate commit

---
+++ HomeStudio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomeStudio
+//
+//  Created by Sachin Bhardwaj on 24/10/15.
+//  Copyright (c) 2015 Sachin Bhardwaj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
