9839b0cf6864d55fd8e261d361ce5453c619f54b
Sachin Bhardwaj <sachin007@Sachins-MacBook-Pro.local>
2015-11-21 20:47:59 +0530

    Page controller added

---
+++ HomeStudio/MVC/Controllers/PageContentViewController/PageContentViewController.h
@@ -0,0 +1,17 @@
+//
+//  PageContentViewController.h
+//  PageViewDemo
+//
+//  Created by abc on 18/02/15.
+//  Copyright (c) 2015 com.TheAppGuruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageContentViewController : UIViewController
+
+@property  NSUInteger pageIndex;
+@property  NSString *imgFile;
+@property  NSString *txtTitle;
+@property (weak, nonatomic) IBOutlet UIImageView *ivScreenImage;
+@end
